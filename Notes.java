e187f4849de83cccecb5473b6f26bad1003cec59
SreekarB17 <80225298+SreekarB17@users.noreply.github.com>
2025-11-17 13:36:35 -0500

    first

---
+++ Notes.java
@@ -0,0 +1,10 @@
+public class Notes {
+
+	public static void main(String[] args) {
+		System.out.println("My name is steven schiff");
+	}
+
+	public void printName(String name) {
+		System.out.println(name);
+	}
+}
